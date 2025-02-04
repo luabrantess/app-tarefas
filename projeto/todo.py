fb9c6006f81dda41509086c4c0b5e956a7eacf69
Ana Luiza <100938920+luabrantess@users.noreply.github.com>
2025-02-04 20:02:22 +0000

    Criaçao do Tabs

--- projeto/todo.py
+++ projeto/todo.py
@@ -8,17 +8,28 @@ class ToDo:
         self.page.window_height = 450
         self.page.window_resizable = False
         self.page.window_always_on_top = True
-        self.page.title = "Tarefas do dia"
+        self.page.title = 'Tarefas do dia'
         self.main_page()
 
     def main_page(self):
-        input_task = ft.TextField(hint_text = "Digite uma tarefa")
+        input_task = ft.TextField(hint_text = "Digite uma tarefa", expand=True)
 
         input_bar = ft.Row(
-            controls={
+            controls=[
                 input_task,
                 ft.FloatingActionButton(icon=ft.icons.ADD)
-            }
+            ]
         )
+
+        tabs = ft.Tabs(
+            selected_index=0,
+            tabs=[
+                ft.Tab(text='Todos'),
+                ft.Tab(text='Em andamento'),
+                ft.Tab(text='Finalizados')
+            ]
+        )
+
+        self.page.add(input_bar, tabs)
 
 ft.app(target = ToDo)
