aa9b152c288d9fb9559c890faee42b36a8db3ea4
Ana Luiza <100938920+luabrantess@users.noreply.github.com>
2025-02-04 20:31:59 +0000

    Criação do banco de dados

--- projeto/todo.py
+++ projeto/todo.py
@@ -1,4 +1,5 @@
 import flet as ft
+import sqlite3
 
 class ToDo:
     def __init__(self, page:ft.Page):
@@ -9,15 +10,46 @@ class ToDo:
         self.page.window_resizable = False
         self.page.window_always_on_top = True
         self.page.title = 'Tarefas do dia'
+        self.task = ''
+        self.db_execute('CREATE TABLE IF NOT EXISTS tasks(name, status)')
         self.main_page()
 
+    def db_execute(self,query,params = []):
+        with sqlite3.connect('database.db') as con:
+            cur = con.cursor()
+            cur.execute(query, params)
+            con.commit()
+            return cur.fetchall()
+
+    def set_value(self,e):
+        self.task = e.control.value
+
+    def add(self,e,input_task):
+        name = self.task
+        status = 'incomplete'
+
+        if name:
+            self.db_execute(query='INSERT INTO tasks VALUE(?,?)', params=(name, status))
+            input_task.value = ''
+            
+
+    def tasks_container(self):
+        return ft.Container(
+            height=self.page.height * 0.8,
+            content = ft.Column(
+                controls = [
+                    ft.Checkbox(label='Tarefa 1', value = True)
+                ]
+            )
+        )
+
     def main_page(self):
-        input_task = ft.TextField(hint_text = "Digite uma tarefa", expand=True)
+        input_task = ft.TextField(hint_text = "Digite uma tarefa", expand=True, on_change=self.set_value)
 
         input_bar = ft.Row(
             controls=[
                 input_task,
-                ft.FloatingActionButton(icon=ft.icons.ADD)
+                ft.FloatingActionButton(icon=ft.icons.ADD, on_click=lambda e: self.add(e,input_task))
             ]
         )
 
@@ -30,6 +62,8 @@ class ToDo:
             ]
         )
 
-        self.page.add(input_bar, tabs)
+        tasks = self.tasks_container()
+
+        self.page.add(input_bar, tabs, tasks)
 
 ft.app(target = ToDo)
